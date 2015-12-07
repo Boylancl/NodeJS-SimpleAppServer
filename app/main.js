4b903e7317401ad6518795f7e6badb6e6e65e37d
Clayton Boylan <Boylancl@gmail.com>
2015-12-07 17:01:25 -0600

    Updates to App Main - DOES NOT RUN

--- app/main.js
+++ app/main.js
@@ -5,12 +5,13 @@ module.exports = {
   {
     //init Event Handlers
     for(var i=0; i<appDef.EventHandlers.length; i++){
-      //Hook the Event Handlers to the Server Events
-      appDef.ParentEvents.on(appDef.EventHandlers[i].EventName
-        ,appDef.EventHandlers[i].Handlers[i]);
-      //Pass a ref to the App Events
       appDef.EventHandlers[i].OutEvents = appDef.EventEmitter;
-            console.log(appDef.EventHandlers[i].OutEvents);
+
+      for(var j=0; j<appDef.EventHandlers[i].Handlers.length; j++){
+        //Hook the Event Handlers to the Server Events
+        appDef.ParentEvents.on(appDef.EventHandlers[i].EventName
+          ,appDef.EventHandlers[i].Handlers[j]);
+      }
     }
   }
 
