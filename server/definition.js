8d89262c1881b8bae727d033152ec020edb1bea5
Clayton Boylan <boylancl@gmail.com>
2015-12-18 19:16:30 -0600

    Refactoring Server Module -- DOES NOT RUN

--- server/definition.js
+++ server/definition.js
@@ -4,24 +4,13 @@ var onRequestStart = require('./events/onRequestStart.js');
 module.exports = function(config){
   moduleDef.call(this, config);
 
-  //Set variables and members
-  this.server = config.server;
   var instance = this;
 
-  //override Routing info
-  this.routes.in = {
-    'request' : function(request, response){
-      //Hook actions to process the request
-      for(var i=0; i< onRequestStart.length; i++){
-        onRequestStart[i].call(instance, request, response);
-      }
-    }
-  };
 
   //Hook the in route to the in channel
   this.listenOn(this.routes.in, this.channels.in);
 
   //Start Server
-  this.server.listen(config.port, config.hostname);
+  this.parent..
 
 };
--- server/main.js
+++ server/main.js
@@ -4,12 +4,6 @@ const config = require('./config.js');
 const server = new definition(config);
 console.log('Server Started!');
 
-var hostname = config.hostname;
-if(hostname == undefined){
-  hostname = 'Localhost';
-}
-console.log('Listening on %s:%s', hostname, config.port);
-
 module.exports = {
   channels: {
     out : server.channels.out
