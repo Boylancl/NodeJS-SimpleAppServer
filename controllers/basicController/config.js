9556f95632f3209441a00c1ec61e4efea48a40ed
Clayton Boylan <Boylancl@gmail.com>
2016-09-14 12:36:08 -0500

    revert this

--- controllers/basicController/config.js
+++ controllers/basicController/config.js
@@ -4,16 +4,18 @@ const onBasicRequest = require('./events/onBasicRequest.js');
 module.exports = function(){
   config.call(this); //inherit
 
-  this.name = "basicCntrlr";
+  this.channels = this.channels.concat({
+    'WebRequest': undefined
+  });
 
-  this.parent = require('../../app/main.js');
+  this.name = "basicCntrlr";
 
   this.tier = 2;
   this.routes = this.routes.concat([
       {
         name: this.name
         ,actions: onBasicRequest
-        ,listenOn: this.parent.channels.out
+        ,listenOn: 'WebRequest'
       }
   ]);
 
